c889ec2ba8f1f69f522a3fcb875990c939e88840
TheTitanRain <151706424+trmatter@users.noreply.github.com>
2025-10-01 08:54:43 +1000

    Update database driver in init.go to use modernc.org/sqlite for improved compatibility

--- db/init.go
+++ db/init.go
@@ -3,11 +3,11 @@ package db
 import (
 	"database/sql"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 func Init(path string) error {
-	db, err := sql.Open("sqlite3", path)
+	db, err := sql.Open("sqlite", path)
 	if err != nil {
 		return err
 	}
